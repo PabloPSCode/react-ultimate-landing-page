cee3303cc495964325850de0e1731f6d4d85d3c0
Pablo <pablolucio_@hotmail.com>
2026-01-22 13:49:55 -0300

    feat: creates GlbViewer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import applyGoogleTranslateDOMPatch from "@/utils/applyGoogleTranslateDOMPatch";
 import { useEffect } from "react";
 import ComponentsSection from "./components/ComponentsSection";
+import FaqSection from "./components/FaqSection";
 import FeaturesSection from "./components/FeaturesSection";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -10,7 +11,6 @@ import PurchaseSection from "./components/PurchaseSection";
 import SuccessCasesSection from "./components/SuccessCasesSection";
 import TestimonialsSection from "./components/TestimonialsSection";
 import TopSection from "./components/TopSection";
-import FaqSection from "./components/FaqSection";
 
 export default function Home() {
   useEffect(() => {
